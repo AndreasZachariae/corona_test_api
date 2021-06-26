6d996ca997bef4afc53b4b51c68440a57a160bf8
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2021-06-26 23:22:08 +0200

    Add test for new routes

--- tests/test_corona_test_api.py
+++ tests/test_corona_test_api.py
@@ -1,6 +1,7 @@
 from corona_test_api.corona_test_api import app
 import json
 
+
 def test_hello_world():
     with app.test_client() as test_app:
         response = test_app.get("/")
@@ -9,11 +10,13 @@ def test_hello_world():
         json_object = json.loads(response.data.decode())
         assert json_object == {"response": "Hello World!"}
 
+
 def test_sum():
     with app.test_client() as test_app:
         response = test_app.get("/sum/1/30")
         assert 200 == response.status_code
         assert 31 == int(response.data.decode())
+
 
 def test_json_response():
     with app.test_client() as test_app:
@@ -23,10 +26,31 @@ def test_json_response():
         json_object = json.loads(response.data.decode())
         assert json_object == {
             'key': 'value',
-            "list": [1,2,3]
+            "list": [1, 2, 3]
         }
+
 
 def test_give_input():
     with app.test_client() as test_app:
         response = test_app.get("/input?parameter1=blabla")
         assert 200 == response.status_code
+
+
+def test_get_testresult():
+    with app.test_client() as test_app:
+        response = test_app.get("/testresult?id=1&positive=1")
+        assert 200 == response.status_code
+
+
+def test_get_statistics():
+    with app.test_client() as test_app:
+        response = test_app.get("/statistics")
+        assert 200 == response.status_code
+
+        json_object = json.loads(response.data.decode())
+        assert json_object == {
+            'numberOfTests': 1,
+            "numberOfNegativeTests": 0,
+            "numberOfPositiveTests": 1,
+            "numberOfUniquePersons": 1
+        }
