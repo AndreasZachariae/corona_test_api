308e3802d29100a337679205ef4cf5b2a5f6c9d5
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2021-06-27 17:47:23 +0200

    Cleanup code

--- corona_test_api/corona_test_api.py
+++ corona_test_api/corona_test_api.py
@@ -1,13 +1,22 @@
-import json
+"""API for recording Corona test results and providing a statistic.
+
+Todo:
+    * Database integration
+    * Add date to input data
+"""
+
+# import json
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_webframeworks.flask import FlaskPlugin
 from marshmallow import Schema, fields
-from flask import Flask, abort, request, make_response, jsonify
+from flask import Flask, abort, request
 from corona_test_api.statistics import Statistics
 
 
 class StatisticsShema(Schema):
+    """Marshmallow schema for statistics type
+    """
     numberOfTests = fields.Int()
 
 
@@ -54,20 +63,20 @@ statistics = Statistics()
 
 @app.route("/testresult")
 def get_testresult():
-    id = request.args.get('id', type=str)
+    test_id = request.args.get('id', type=str)
 
     positive = request.args.get('positive', type=bool)
     # egal welcher string, sobald nicht leer wird er true
     # Welche Annahmen darf man treffen 0/1, true/false, True/False
 
-    if (id is None) or (positive is None):
+    if (test_id is None) or (positive is None):
         abort(400, description="Given parameters are not valid or missing." +
-              str(id)+str(positive))
+              str(test_id)+str(positive))
 
-    statistics.add_test(id, positive)
+    statistics.add_test(test_id, positive)
 
     # response code 201 created?
-    return ("new test registered" + str(id)+str(positive))
+    return "new test registered" + str(test_id)+str(positive)
 
 
 @app.route("/statistics")
@@ -75,74 +84,13 @@ def get_statistics():
     return (statistics.get_statistics(), 200)
 
 
-@app.route("/")
-def hello_world():
-    """ Hello World!
-    ---
-    get:
-        description: Hello World!
-        responses:
-            200:
-                description: Succesfully helloed to world!
-                content:
-                    text/plain:
-                        schema:
-                            type: string
-                            example: Hello World!
-    """
-    return {"response": "Hello World!"}
+# with app.test_request_context():
+#     spec.path(view=get_testresult)
+#     spec.path(view=get_statistics)
 
+# with open('specification.json', 'w') as f:
+#     json.dump(spec.to_dict(), f)
 
-@app.route("/sum/<int:number1>/<int:number2>")
-def sum(number1, number2):
-    """Sum of two integers
-    ---
-    get:
-        description: Sum of two integers
-        parameters:
-            - in: path
-              name: number1
-              description: First number to add
-              required: true
-              schema:
-                type: integer
-            - in: path
-              name: number2
-              description: Second number to add
-              required: true
-              schema:
-                type: integer
-        responses:
-            200:
-                description: Succesfully added both numbers!
-    """
-    return str(number1 + number2)
-
-
-@app.route("/json")
-def json_response():
-    return {
-        'key': 'value',
-        "list": [1, 2, 3]
-    }
-
-
-@app.route("/input")
-def give_input():
-    parameter1 = request.args.get("parameter1")
-    return(parameter1)
-
-
-with app.test_request_context():
-    spec.path(view=hello_world)
-    spec.path(view=sum)
-
-with open('specification.json', 'w') as f:
-    json.dump(spec.to_dict(), f)
-
-#import yaml
-# with open('specification.yml', 'w') as f:
-#    yaml.dump(spec.to_yaml(), f)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- corona_test_api/statistics.py
+++ corona_test_api/statistics.py
@@ -4,15 +4,15 @@ class Statistics(object):
         self.number_of_unique_persons = 0
         self.test_db = []
 
-    def add_test(self, id: str, positive: bool):
+    def add_test(self, test_id: str, positive: bool):
 
-        if all(test["id"] != id for test in self.test_db):
+        if all(test["id"] != test_id for test in self.test_db):
             self.number_of_unique_persons += 1
 
         if positive:
             self.number_of_positive_tests += 1
 
-        self.test_db.append({"id": id, "result": positive})
+        self.test_db.append({"id": test_id, "result": positive})
 
     def get_statistics(self):
         return {"numberOfTests": len(self.test_db),
