5965c749ac2b16ee71aa50742e1c646cf9d92866
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2021-06-27 18:22:02 +0200

    Add docstrings for statistics and tests

--- tests/test_corona_test_api.py
+++ tests/test_corona_test_api.py
@@ -1,14 +1,23 @@
+"""Module for testing the corona test API
+"""
+
 import json
 from corona_test_api.corona_test_api import app
 
 
 def test_get_testresult():
+    """Tests if adding a test result with example data returns with OK value
+    """
+
     with app.test_client() as test_app:
         response = test_app.get("/testresult?id=1&positive=1")
         assert response.status_code == 200
 
 
 def test_get_statistics():
+    """Tests for correct statistics after adding the example test before
+    """
+
     with app.test_client() as test_app:
         response = test_app.get("/statistics")
         assert response.status_code == 200
