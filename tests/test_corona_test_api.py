7dfc7801527f159e01d282ef3a0b0204920e9bd4
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2021-06-29 15:26:30 +0200

    Update test for code 201

--- tests/test_corona_test_api.py
+++ tests/test_corona_test_api.py
@@ -11,7 +11,7 @@ def test_get_testresult():
 
     with app.test_client() as test_app:
         response = test_app.get("/testresult?id=1&positive=1")
-        assert response.status_code == 200
+        assert response.status_code == 201
 
     with app.test_client() as test_app:
         response = test_app.get("/testresult?id=1")
